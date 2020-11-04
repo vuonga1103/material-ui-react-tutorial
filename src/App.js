65230ef60bd2feb4c5c550c8187d091e1aa5bfb3
vuonga1103 <vuonga1103@yahoo.com>
2020-11-03 22:25:43 -0500

    creating a checkbox and label

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Button, ButtonGroup } from "@material-ui/core";
+import { Button, ButtonGroup, FormControlLabel } from "@material-ui/core";
 import SaveIcon from "@material-ui/icons/Save";
 import DeleteIcon from "@material-ui/icons/Delete";
+
+import Checkbox from "@material-ui/core/Checkbox";
+import { useState } from "react";
+
+function CheckboxExample() {
+  const [checked, setChecked] = useState(true);
+
+  return (
+    // Use form control to add label for checkbox
+    <FormControlLabel
+      control={
+        <Checkbox
+          checked={checked}
+          // We can add icons in place of checkbox
+          icon={<SaveIcon />}
+          checkedIcon={<SaveIcon />}
+          onChange={(e) => setChecked(!checked)}
+          // color="primary"
+          inputProps={{
+            "aria-label": "secondary checkbox",
+          }}
+        />
+      }
+      label="Testing Checkbox"
+    />
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <CheckboxExample />
+
         {/* variant prop lets you specify how you want the button to look */}
         <ButtonGroup variant="contained" color="secondary">
           <Button startIcon={<SaveIcon />} href="#">
