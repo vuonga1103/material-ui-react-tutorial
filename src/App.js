c3bfc8e601a3d61ed12520cbafe725654e12c906
vuonga1103 <vuonga1103@yahoo.com>
2020-11-03 22:04:18 -0500

    created a simple button and added @material-ui/icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "@material-ui/core";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {/* variant prop lets you specify how you want the button to look */}
+        <Button
+          href="#"
+          variant="contained"
+          color="secondary"
+          size="small"
+          // disabled
+          // style={{ fontSize: 24 }}
+        >
+          Hello World
+        </Button>
+
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
